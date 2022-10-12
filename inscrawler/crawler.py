77a2b369872b71ce0471896c30c1df174e6d1e52
kneeraazon01 <kneeraazon@gmail.com>
2022-10-12 19:49:40 +0545

    instagram resources collection

--- inscrawler/crawler.py
+++ inscrawler/crawler.py
@@ -12,19 +12,19 @@ from time import sleep
 
 from tqdm import tqdm
 
-from . import secret
-from .browser import Browser
-from .exceptions import RetryException
-from .fetch import fetch_caption
-from .fetch import fetch_comments
-from .fetch import fetch_datetime
-from .fetch import fetch_imgs
-from .fetch import fetch_likers
-from .fetch import fetch_likes_plays
-from .fetch import fetch_details
-from .utils import instagram_int
-from .utils import randmized_sleep
-from .utils import retry
+from inscrawler import secret
+from inscrawler.browser import Browser
+from inscrawler.exceptions import RetryException
+from inscrawler.fetch import fetch_caption
+from inscrawler.fetch import fetch_comments
+from inscrawler.fetch import fetch_datetime
+from inscrawler.fetch import fetch_imgs
+from inscrawler.fetch import fetch_likers
+from inscrawler.fetch import fetch_likes_plays
+from inscrawler.fetch import fetch_details
+from inscrawler.utils import instagram_int
+from inscrawler.utils import randmized_sleep
+from inscrawler.utils import retry
 
 
 class Logging(object):
